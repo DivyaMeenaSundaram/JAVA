7a6acb8831bbfed0ce5c5a7b062790ce0cc5f22d
Divya Meena <53110268+DivyaMeenaSundaram@users.noreply.github.com>
2023-02-24 11:34:28 +0530

    Add files via upload

---
+++ Boolean1.java
@@ -0,0 +1,11 @@
+class Boolean1{
+public static void main(String args[]){
+
+double d=10.0;
+int i=(int) d;
+
+System.out.println(d);
+System.out.println(i);
+}
+}
+
---
+++ BooleanEx.java
@@ -0,0 +1,10 @@
+class BooleanEx{
+public static void main(String args[]){
+int x=10, y=11, z=100;
+System.out.println(x>y);
+System.out.println(y>z);
+
+boolean isJavaFun=false;
+System.out.println(isJavaFun);
+}
+}
