5a5e5309aae10d341f110eae7eadf8cdaca670df
Divya Meena <53110268+DivyaMeenaSundaram@users.noreply.github.com>
2023-02-25 14:27:01 +0530

    Add files via upload

---
+++ Ar1.java
@@ -0,0 +1,12 @@
+// Program to illustarte length() function of string and length property of arrays
+
+class Ar1{
+public static void main(String args[])
+{
+String[] s1={"Divya", "Meena", "Sundaram"};
+System.out.println(s1.length);
+
+String s2 = "Divya Meena";
+System.out.println(s2.length());
+}
+}
