e937a81afe08ba078a8a72e20b0f424ccbec4bb0
Divya Meena <53110268+DivyaMeenaSundaram@users.noreply.github.com>
2023-02-25 14:43:48 +0530

    Add files via upload

---
+++ AddUsingScanner.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class AddUsingScanner{
+public static void main(String args[]){
+Scanner s1 = new Scanner(System.in);
+System.out.println("Enter your 1st number");
+int a = s1.nextInt();
+System.out.println("Enter your 2nd number");
+int b = s1.nextInt();
+System.out.println("Enter your 3rd number");
+int c = s1.nextInt();
+int d = a+b+c;
+System.out.println("Addition of 3 numbers is "+d);
+}
+}
+
